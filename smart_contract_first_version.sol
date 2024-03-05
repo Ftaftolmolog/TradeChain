d4936852c7ebe3789fef4156d673bad30e71f212
Ftaftolmolog <76704003+Ftaftolmolog@users.noreply.github.com>
2024-03-05 15:48:35 +0300

    Update smart_contract_first_version.sol

--- smart_contract_first_version.sol
+++ smart_contract_first_version.sol
@@ -4,63 +4,42 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Vault is ERC20 {
+contract TradeChain is ERC20 {
     IERC20 public immutable token;
 
-    constructor(IERC20 token_) ERC20("Liquidity Provider Token", "LP-TKN") {
+    constructor(IERC20 token_) ERC20("TradeChain-Stable-coin", "TCC") {
         token = token_;
     }
 
-    function deposit(uint256 amount) external {
+    function invest(uint256 amount) external {
         require(amount > 0);
 
-        uint256 amountToken = amount;
-        uint256 supplyLPToken = this.totalSupply();
+        uint256 amountInvested = amount;
+        uint256 supplyStableCoins = this.totalSupply();
         uint256 balanceToken = token.balanceOf(address(this));
 
-        uint256 amountLPToken;
-        if (supplyLPToken == 0) {
-            amountLPToken = amountToken;
+        uint256 amountStable;
+        if (supplyStableCoins == 0) {
+            amountStable = amountInvested;
         } else {
-            amountLPToken = (amountToken * supplyLPToken) / balanceToken;
+            amountStable = (amountInvested * supplyStableCoins) / balanceToken;
         }
 
-        _mint(msg.sender, amountLPToken);
-        token.transferFrom(msg.sender, address(this), amountToken);
+        _mint(msg.sender, amountStable);
+        token.transferFrom(msg.sender, address(this), amountInvested);
     }
 
-    function withdraw(uint256 amount) external {
+    function divest(uint256 amount) external {
         require(amount > 0);
 
-        uint256 amountLPToken = amount;
-        uint256 supplyLPToken = this.totalSupply();
+        uint256 amountStable = amount;
+        uint256 supplyStableCoins = this.totalSupply();
         uint256 balanceToken = token.balanceOf(address(this));
 
-        uint256 amountToken = (amountLPToken * balanceToken) / supplyLPToken;
+        uint256 amountDivested = (amountStable * balanceToken) / supplyStableCoins;
 
-        _burn(msg.sender, amountLPToken);
-        token.transfer(msg.sender, amountToken);
+        _burn(msg.sender, amountStable);
+        token.transfer(msg.sender, amountDivested);
     }
 
 }
-
-// contract TradeChain {
-//     address public owner;
-//     uint256 public balance;
-
-//     constructor() {
-//         owner = msg.sender; // public address
-
-//     }
-
-//     receive() external payable {
-//         balance += msg.value;
-//      }
-
-//     function withdraw(uint amount, address payable destAddr) public {
-//         require(msg.sender == owner, "Only owner");
-//         require(amount <= balance, "Insufficeint funds");
-//         destAddr.transfer(amount);
-//         balance -= amount;
-//     }
-// }
